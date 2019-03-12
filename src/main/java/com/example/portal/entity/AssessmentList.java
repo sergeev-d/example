51635106037b7d043ac52feaea442a2cbf5f63e1
Denis Sergeev <dxsergeevsxd@mail.ru>
2019-03-12 08:49:21 +0300

    --

--- src/main/java/com/example/portal/entity/AssessmentList.java
+++ src/main/java/com/example/portal/entity/AssessmentList.java
@@ -6,24 +6,24 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class AssessmentList {
     private List<AssessmentPreview> assessments;
-    private AtomicInteger assessmentCount;
+    private AtomicInteger assessmentsCount;
 
     public AssessmentList() {
         this.assessments = new ArrayList<>();
-        this.assessmentCount = new AtomicInteger(0);
+        this.assessmentsCount = new AtomicInteger(0);
     }
 
     public List<AssessmentPreview> getAssessments() {
         return assessments;
     }
 
-    public int getAssessmentCount() {
-        return assessmentCount.get();
+    public int getAssessmentsCount() {
+        return assessmentsCount.get();
     }
 
     public void addAssessmentPreview(AssessmentPreview assessmentPreview){
         assessments.add(assessmentPreview);
-        assessmentCount.addAndGet(1);
+        assessmentsCount.addAndGet(1);
     }
 }
 
--- src/main/java/com/example/portal/handler/AssessmentsHandler.java
+++ src/main/java/com/example/portal/handler/AssessmentsHandler.java
@@ -7,12 +7,26 @@ import io.vertx.core.json.Json;
 import io.vertx.ext.web.RoutingContext;
 import org.apache.http.HttpStatus;
 
+import java.util.Arrays;
+
 public class AssessmentsHandler implements Handler<RoutingContext> {
     @Override
     public void handle(RoutingContext ctx) {
         final AssessmentList assessmentList = new AssessmentList();
-        assessmentList.addAssessmentPreview(new AssessmentPreview());
-        assessmentList.addAssessmentPreview(new AssessmentPreview());
+        final AssessmentPreview ap1 = new AssessmentPreview();
+        ap1.setId(1l);
+        ap1.setName("Test efficiency");
+        ap1.setDescription("Test efficiency for small company");
+        ap1.setApplicationFields(Arrays.asList("Filed area2","Filed area2"));
+
+        final AssessmentPreview ap2 = new AssessmentPreview();
+        ap2.setId(2l);
+        ap2.setName("Test efficiency2");
+        ap2.setDescription("Test efficiency for small company");
+        ap2.setApplicationFields(Arrays.asList("Filed area1","Filed area2","Filed area3"));
+
+        assessmentList.addAssessmentPreview(ap1);
+        assessmentList.addAssessmentPreview(ap2);
 
         ctx.response()
                 .setStatusCode(HttpStatus.SC_OK)
