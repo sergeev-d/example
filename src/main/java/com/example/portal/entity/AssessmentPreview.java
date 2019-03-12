4fba404ad1c851f3aba84b793746fcc1b7275ee7
Denis Sergeev <dxsergeevsxd@mail.ru>
2019-03-12 08:48:49 +0300

    --

--- src/main/java/com/example/portal/entity/AssessmentPreview.java
+++ src/main/java/com/example/portal/entity/AssessmentPreview.java
@@ -1,7 +1,53 @@
 package com.example.portal.entity;
 
+import java.util.List;
+
 public class AssessmentPreview {
+    private long id;
+    private String name;
+    private String description;
+    private List<String> applicationFields;
+
 
     public AssessmentPreview() {
+    }
+
+    public AssessmentPreview(long id, String name, String description, List<String> applicationFields) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.applicationFields = applicationFields;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getApplicationFields() {
+        return applicationFields;
+    }
+
+    public void setApplicationFields(List<String> applicationFields) {
+        this.applicationFields = applicationFields;
     }
 }
